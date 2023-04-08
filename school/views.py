2a91aef7f1047c05b37e7d95bddb62ee610247ad
TimofeevAndrei <i.pika4u@gmail.com>
2023-04-09 01:43:50 +0700

    views.py

--- school/views.py
+++ school/views.py
@@ -3,13 +3,15 @@ from django.shortcuts import render
 
 from .models import Student
 
-
+class PublisherListView(ListView):
+    model = Student
 def students_list(request):
+    object_list = Student.objects.all()
     template = 'school/students_list.html'
-    context = {}
-
+    context = {'students': object_list}
+    print(context)
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
     ordering = 'group'
 
-    return render(request, template, context)
+    return render(request, template, context=context)
